a47521c4d432ce89b6e9fe62acc83cb3ca9f15dd
Kundan <kundanss22@gmail.com>
2025-02-16 09:47:10 +0530

    dragon link fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -144,6 +144,7 @@ export function HeaderMegaMenu() {
               className={classes.link}
               type="button"
               underline={false}
+              onClick={closeDrawer}
             >
               Dragon
             </Anchor>
